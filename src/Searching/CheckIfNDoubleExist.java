cdd6d5d85a66526f6ee22fbf094d60bc0c69bf7c
SumritSingh15 <sumrit578singh@gmail.com>
2026-05-12 20:19:27 +0530

    1346. Check If N and Its Double Exist

---
+++ src/Searching/CheckIfNDoubleExist.java
@@ -0,0 +1,20 @@
+package Searching;
+
+public class CheckIfNDoubleExist {
+    public static void main(String[] args) {
+        int [] arr = {10,2,5,3};
+        boolean ans = checkIfExist(arr);
+        System.out.println(ans);
+
+    }
+    public static boolean checkIfExist(int[] arr) {
+
+        for(int i = 0; i< arr.length;i++){
+            for(int j =0; j< arr.length;j++){
+                if(i !=j && arr[i]== 2*arr[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
