0c54da4b5e9175972c129809ed269ba775cb83f5
SumritSingh15 <sumrit578singh@gmail.com>
2026-05-16 21:12:44 +0530

    Two Sum

---
+++ src/Searching/TwoSum.java
@@ -0,0 +1,25 @@
+package Searching;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int [] nums = {2,7,11,15};
+        int target = 9;
+        int [] ans = twoSum(nums,target);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int need = target - nums[i];
+            if(map.containsKey(need)){
+                return new int[]{map.get(need),i};
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+    }
+}
