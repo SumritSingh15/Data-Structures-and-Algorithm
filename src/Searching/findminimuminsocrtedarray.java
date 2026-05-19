12416245c9cef3d3672bbb95a2dff585e8a2c370
SumritSingh15 <sumrit578singh@gmail.com>
2026-05-19 23:13:56 +0530

    153. Find Minimum in Rotated Sorted Array

---
+++ src/Searching/findminimuminsocrtedarray.java
@@ -0,0 +1,32 @@
+package Searching;
+
+public class findminimuminsocrtedarray {
+    public static void main(String[] args) {
+        int [] nums ={3,4,5,1,2};
+        int ans  = findMin(nums);
+        System.out.println(ans);
+    }
+    public static int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start < end){
+
+            int mid = start + (end - start)/2;
+
+            // minimum right side me
+            if(nums[mid] > nums[end]){
+
+                start = mid + 1;
+            }
+
+            // minimum left side me
+            else{
+
+                end = mid;
+            }
+        }
+
+        return nums[start];
+    }
+}
