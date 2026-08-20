b9fc225c6c5232f3ba24f87b06bf0c5196a8e93d
SumritSingh15 <sumrit578singh@gmail.com>
2026-08-20 23:24:45 +0530

    long pressed

---
+++ src/Strings/LongPressed.java
@@ -0,0 +1,30 @@
+package Strings;
+
+public class LongPressed {
+    public static void main(String[] args) {
+String name = "alex";
+String typed = "allx";
+boolean ans = isLongPressedName(name,typed);
+        System.out.println(ans);
+    }
+    public static boolean isLongPressedName(String name, String typed) {
+        int i = 0;
+        int j = 0;
+
+        while (j < typed.length()) {
+
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            }
+            else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return i == name.length();
+    }
+}
