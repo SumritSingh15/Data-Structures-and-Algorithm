80a6a671fe4c5c7c1e2dae361fb4ed3bb2ac43a7
SumritSingh15 <sumrit578singh@gmail.com>
2026-08-20 23:29:05 +0530

    firstOccurence

---
+++ src/Strings/FindTheFirstOccurence.java
@@ -0,0 +1,32 @@
+package Strings;
+
+public class FindTheFirstOccurence {
+    public static void main(String[] args) {
+                String haystack = "sadbutsad";
+                String needle = "sad";
+                int ans = strStr(haystack,needle);
+        System.out.println(ans);
+    }
+    public static int strStr(String haystack, String needle) {
+
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            int j = 0;
+
+            while (j < needle.length() &&
+                    haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
